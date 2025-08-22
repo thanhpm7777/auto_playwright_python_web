2efeedd5215b0cd9cfbb98ef067b0fdf3a7804e2
thanhpm7777 <minhthanh.st92@gmail.com>
2025-08-22 21:45:34 +0700

    done

--- pages/post_editor_page.py
+++ pages/post_editor_page.py
@@ -4,28 +4,32 @@ from .base_page import BasePage
 
 
 class PostEditorPage(BasePage):
-    LINK_NEW_ARTICLE = "New Article"
+    LINK_NEW_ARTICLE = "user-avatar"
+    lbl_setting = "Cài đặt thông tin"
+    lbl_dangbai = "Đăng bài"
 
-    PH_TITLE = "Article Title"
-    PH_ABOUT = "What's this article about?"
-    PH_BODY = "Write your article (in markdown)"
+    PH_TITLE = "title"
+    PH_category = "#category_id"
+    PH_BODY = "presentation"
     PH_TAGS = "Enter tags"
 
     BTN_PUBLISH = "Publish Article"
 
     def goto_editor(self):
         with allure.step("Mở form tạo bài viết"):
-            self.click_nav(self.LINK_NEW_ARTICLE)
+            self.get_by_class(self.LINK_NEW_ARTICLE).click()
+            self.click_nav(self.lbl_setting)
+            self.click_link_or_button(self.lbl_dangbai, expect_nav=True)
         return self
 
-    def create_post(self, title: str, about: str, body: str, tags: str):
+    def create_post(self, title, category,body):
         with allure.step("Nhập nội dung bài viết"):
-            self.fill_by_placeholder(self.PH_TITLE, title)
-            self.fill_by_placeholder(self.PH_ABOUT, about)
-            self.fill_by_placeholder(self.PH_BODY, body)
-            self.fill_by_placeholder(self.PH_TAGS, tags)
-        with allure.step("Xuất bản bài viết"):
-            self.click_button(self.BTN_PUBLISH)
+            self.fill_by_name(self.PH_TITLE, title)
+            self.select_by_value(self.PH_category, category)
+            #self.fill_by_placeholder(self.PH_BODY, body)
+            # self.fill_by_placeholder(self.PH_TAGS, tags)
+       # with allure.step("Xuất bản bài viết"):
+            #self.click_button(self.BTN_PUBLISH)
         return self
 
     def assert_post_published(self, title: str):
--- tests/web/test_create_post.py
+++ tests/web/test_create_post.py
@@ -1,23 +1,24 @@
-# import allure
-# from faker import Faker
-# from core.utils import now_slug
-# from pages.post_editor_page import PostEditorPage
-#
-# fake = Faker()
-#
-#
-# @allure.feature("Post")
-# @allure.severity(allure.severity_level.CRITICAL)
-# class TestCreatePost:
-#     @allure.story("Tạo bài viết mới thành công")
-#     @allure.tag("auth")
-#     def test_create_post_success(self, page_authenticated):
-#         page = page_authenticated
-#         pe = PostEditorPage(page)
-#
-#         title = now_slug("how-to-playwright")
-#         about = fake.sentence(nb_words=6)
-#         body = "\n\n".join(fake.paragraphs(nb=3))
-#         tags = "playwright,python,e2e"
-#
-#         pe.open().goto_editor().create_post(title, about, body, tags).assert_post_published(title)
+import allure
+from faker import Faker
+from core.utils import now_slug
+from pages.post_editor_page import PostEditorPage
+from pages.login_page import LoginPage
+from core.config import settings
+import pytest
+fake = Faker()
+
+@pytest.mark.smoke
+@allure.feature("Post")
+@allure.severity(allure.severity_level.CRITICAL)
+class TestCreatePost:
+    @allure.story("Tạo bài viết mới thành công")
+    @allure.tag("auth")
+    def test_create_post_success(self, page):
+        lp = LoginPage(page)
+        lp.goto().login(settings.USER_EMAIL, settings.USER_PASSWORD).assert_logged_in("test1111")
+        pe = PostEditorPage(page)
+
+
+        #pe.open().goto_editor().create_post(title, about, body, tags).assert_post_published(title)
+        pe.open().goto_editor()
+        pe.create_post("abcd", "15", "1234")
